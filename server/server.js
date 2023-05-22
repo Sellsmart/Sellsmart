2792ff01635afabd11791430bafc154c202dcc83
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-05-22 21:29:04 +0200

    Update server.js to now itself better

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.post('/', async (req, res) => {
 
     const SectionResponse = await openai.createCompletion({
       model: "text-davinci-002",
-      prompt: `"You are a chatbot on a website that sales houses. Is this question: " + ${prompt} + "a Normal-Chat-, general-, Contact-, feature-, competition-, products-, shipment-, Website-instruction-, Family- or Product-related one? Respond with one word` ,
+      prompt: `"Is this question: " + ${prompt} + "a Normal-Chat-, general-, Contact-, feature-, competition-, products-, shipment-, Website-instruction-, Family- or Product-related one? Respond with one word` ,
       temperature: 0.5, // Higher values means the model will take more risks.
       max_tokens: 255, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
      
@@ -86,7 +86,7 @@ app.post('/', async (req, res) => {
     else{
       const response = await openai.createCompletion({
         model: "text-babbage-001",
-        prompt: `"Answer in full sentences. You are a chatbot on a business website presenting a real estate agency online. Answer this question: " + ${prompt} + "using these information from a manual: " + ${dataset}` ,
+        prompt: `"Answer in full sentences. You are a chatbot on a marketing website presenting yourself online. Answer this question: " + ${prompt} + "using these information from a manual: " + ${dataset}` ,
         temperature: 0.2, // Higher values means the model will take more risks.
         max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
        
