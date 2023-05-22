9a69d9da47b666711e9c2f4fb1721e2953f6a89d
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-05-22 14:19:19 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async(e) =>{
 
   // fetch data from server
 
-  const response = await fetch('http://localhost:5102', {
+  const response = await fetch('https://sellsmart.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
