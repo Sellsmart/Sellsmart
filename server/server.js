997fc1ea56c6e28d4d937d9fba687170b35c7774
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-05-22 18:30:54 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -52,13 +52,11 @@ app.post('/', async (req, res) => {
     }
     const response = await openai.createCompletion({
       model: "text-babbage-001",
-      prompt: `"You are a chatbot on a business website presenting a real estate agency online. Answer this question: " + ${prompt} + "using these information from a manual: " + ${dataset}` ,
-      temperature: 0.1, // Higher values means the model will take more risks.
-      max_tokens: 255, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      prompt: `"Answer in full sentences. You are a chatbot on a business website presenting a real estate agency online. Answer this question: " + ${prompt} + "using these information from a manual: " + ${dataset}` ,
+      temperature: 0.2, // Higher values means the model will take more risks.
+      max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
      
-      /*frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    */});
+      });
     console.log(response.data.choices[0].message);
     res.status(200).send({
       bot: response.data.choices[0].text
