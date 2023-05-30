9f9240afe9d8ecae49938a4911614a1f12d9b55f
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-05-30 14:35:48 +0200

    Update server.js to make small chunks of information

--- server/server.js
+++ server/server.js
@@ -29,12 +29,37 @@ app.post('/', async (req, res) => {
   try {
     
     const prompt = req.body.prompt;
-    const first = "Act like a chatbot on a website that helps the customer buy something from that specific website. Dont mention yourself. Answer as short as possible. Dont ever provide information you dont know about, instead tell the customer that you dont know and offer alternatives. Act kind and try (if gently possible) to convert the customer into a buying one. This is his question: " + prompt + " Answer it using this data about the busines: Our Chatbot is one of the most advanced AI Chatbots out there. Super functional chatbot that help converts customer much faster and with much better experience. Easy to use, don't need anymore staff + no extra staff cost. works 24/7 + extremely good because it knows you're website very well. Easy to impliment + adapt to your design of your website. Compared to other website, this chatbot is: faster, can answer questions that other chatbot can't. It is also trained to be nice to increase conversion. 3 models/ prices: Start: Fully working chatbot, Easy implementation, Chatbot trained on website, 550$. Pro: Same as start but with Custom chatbot design, 800$. Same as Starter but with Custom Chatbot design and extra features., 3200$. If they contact we can talk about discounts. Product shipment: around 2-3 days. No accessoires needed. Fully personalized. Infinite lifespan. Contact: Email: felixwolny1@gmail.com, phone: none";
+    const first = "Act like a chatbot on a website that helps the customer buy something from that specific website. Dont mention yourself. Answer as short as possible. Dont ever provide information you dont know about, instead tell the customer that you dont know and offer alternatives. Act kind and try (if gently possible) to convert the customer into a buying one. This is his question: " + prompt + " Answer it using this data about the busines:" ;
     console.log("Is starting");
+    const information = "";
+
+    const sectionCheck = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Sort this request " + prompt + " into one of these categories: advantages, different models, Contact, Discounts, Shipment. Respond with only the section name",
+      temperature: 0.1,
+    })
+const answer = sectionCheck.data.choices[0].text;
+    if(answer.includes("advantages")){
+    const first = "Act like a chatbot on a website that helps the customer buy something from that specific website. Dont mention yourself. Answer as short as possible. Dont ever provide information you dont know about, instead tell the customer that you dont know and offer alternatives. Act kind and try (if gently possible) to convert the customer into a buying one. This is his question: " + prompt + " Answer it using this data about the busines:" ;
+    information = " Our Chatbot is one of the most advanced AI Chatbots out there. Super functional chatbot that help converts customer much faster and with much better experience. Easy to use, don't need anymore staff + no extra staff cost. works 24/7 + extremely good because it knows you're website very well. Easy to impliment + adapt to your design of your website. Compared to other website, this chatbot is: faster, can answer questions that other chatbot can't. It is trained to be nice to increase conversion. ";
+    }
+    else if(answer.includes("models")){
+      information = " 3 models/ prices: Start: Fully working chatbot, Easy implementation, Chatbot trained on website, 550$. Pro: Same as start but with Custom chatbot design, 800$. Same as Starter but with Custom Chatbot design and extra features., 3200$.";
+    }
+    else if(answer.includes("Contact")){
+      information = " Contact: Email: felixwolny1@gmail.com, phone: none";
+    }
+    else if(answer.includes("Shipment")){
+      information =" Product shipment: around 2-3 days.";
+    }
+    else if(answer.includes("Discount")){
+      information=" If you contact we can talk about discounts.";
+    }
+
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
-        {role: "user", content: first},
+        {role: "user", content: first + information},
     ] 
       
   
