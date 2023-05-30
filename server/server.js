030d1a7083f67a95a46e17175ed6a6ef902a21fc
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-05-30 14:41:39 +0200

    Update server.js to not have errors anymore

--- server/server.js
+++ server/server.js
@@ -31,11 +31,11 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
     const first = "Act like a chatbot on a website that helps the customer buy something from that specific website. Dont mention yourself. Answer as short as possible. Dont ever provide information you dont know about, instead tell the customer that you dont know and offer alternatives. Act kind and try (if gently possible) to convert the customer into a buying one. This is his question: " + prompt + " Answer it using this data about the busines:" ;
     console.log("Is starting");
-    const information = "";
+    let information = "";
 
     const sectionCheck = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "Sort this request " + prompt + " into one of these categories: advantages, different models, Contact, Discounts, Shipment. Respond with only the section name",
+      prompt: "Sort this request " + prompt + " into one of these categories: advantages, different Models, Products, Contact, Discounts, Shipment. Respond with only the section name",
       temperature: 0.1,
     })
 const answer = sectionCheck.data.choices[0].text;
@@ -43,14 +43,17 @@ const answer = sectionCheck.data.choices[0].text;
     const first = "Act like a chatbot on a website that helps the customer buy something from that specific website. Dont mention yourself. Answer as short as possible. Dont ever provide information you dont know about, instead tell the customer that you dont know and offer alternatives. Act kind and try (if gently possible) to convert the customer into a buying one. This is his question: " + prompt + " Answer it using this data about the busines:" ;
     information = " Our Chatbot is one of the most advanced AI Chatbots out there. Super functional chatbot that help converts customer much faster and with much better experience. Easy to use, don't need anymore staff + no extra staff cost. works 24/7 + extremely good because it knows you're website very well. Easy to impliment + adapt to your design of your website. Compared to other website, this chatbot is: faster, can answer questions that other chatbot can't. It is trained to be nice to increase conversion. ";
     }
-    else if(answer.includes("models")){
+    else if(answer.includes("Models")){
+      information = " 3 models/ prices: Start: Fully working chatbot, Easy implementation, Chatbot trained on website, 550$. Pro: Same as start but with Custom chatbot design, 800$. Same as Starter but with Custom Chatbot design and extra features., 3200$.";
+    }
+    else if (answer.includes("Product")){
       information = " 3 models/ prices: Start: Fully working chatbot, Easy implementation, Chatbot trained on website, 550$. Pro: Same as start but with Custom chatbot design, 800$. Same as Starter but with Custom Chatbot design and extra features., 3200$.";
     }
     else if(answer.includes("Contact")){
       information = " Contact: Email: felixwolny1@gmail.com, phone: none";
     }
     else if(answer.includes("Shipment")){
-      information =" Product shipment: around 2-3 days.";
+      information = " Product shipment: around 2-3 days.";
     }
     else if(answer.includes("Discount")){
       information=" If you contact we can talk about discounts.";
