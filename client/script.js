13e2f327ff5d1bc33181b1ce7df405ac5c0656b5
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-06-02 15:06:25 +0200

    Update script.js to not fetch localhost :-)

--- client/script.js
+++ client/script.js
@@ -177,7 +177,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:5102', {
+  const response = await fetch('https://sellsmart.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
