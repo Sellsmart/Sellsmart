abb94ef661d86a4509aeb1736db5d192dc4c339e
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-06-23 20:43:32 +0200

    Update script.js to fetch sellsmart bro

--- client/script.js
+++ client/script.js
@@ -328,7 +328,7 @@ lastInput = data.get('prompt');
   else{
     promptToSend = data.get('prompt');
   }
-  const response = await fetch('http://localhost:5102', {
+  const response = await fetch('https://sellsmart.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
